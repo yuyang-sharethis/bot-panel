486b76f78ddc31f46c78692f47453ddcebd1d8de
Yuyang Dong <yuyang@sharethis.com>
2023-08-14 03:54:06 -0400

    update bot_init.sh

--- bot_init.sh
+++ bot_init.sh
@@ -7,5 +7,5 @@ echo "Waiting for instance launching...(this will take ~60s)"
 sleep 60
 echo "SSH into bot panel: 'i-0980565bcf5d16d2e' through '$public_dns'"
 chmod 400 bot_panel.pem
-ssh -o "StrictHostKeyChecking no" -i "bot_panel.pem" ubuntu@$public_dns
+ssh -o "StrictHostKeyChecking no" -i "bot_panel.pem" admin@$public_dns
 
