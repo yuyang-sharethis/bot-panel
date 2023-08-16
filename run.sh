c0c8e471032a0745dcc76bd005e404f525a05d0b
Yuyang Dong <yuyang@sharethis.com>
2023-08-16 00:26:10 -0400

    modify cookie

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
-web_driver_path=$(pwd)/chromedriver
+WEB_DRIVER_PATH=$(pwd)/chromedriver
 CHROME_USER_DATA_PATH=$(pwd)/chrome_user_data
 
-python3 bot.py $web_driver_path $CHROME_USER_DATA_PATH
+python3 bot.py $WEB_DRIVER_PATH $CHROME_USER_DATA_PATH
